bbe25dd4468d12bf811ccc7951cbe03e3eae3c44
lea haimovich <haimovit@g.jct.ac.il>
2021-03-21 09:51:42 +0200

    Vector test-begining

---
+++ src/primitives/VectorTest.java
@@ -0,0 +1,77 @@
+package primitives;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static primitives.Util.isZero;
+
+/**
+ * Test for Vector
+ * @author shulamit and lea
+ */
+class VectorTest {
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void substract() {
+    }
+
+    @Test
+    void scale() {
+    }
+
+    /**
+     * Test method for {@link primitives.Vector#crossProduct(primitives.Vector)}.
+     */
+    @Test
+    public void CrossProduct() {
+        Vector v1 = new Vector(1, 2, 3);
+
+        // ============ Equivalence Partitions Tests ==============
+        Vector v2 = new Vector(0, 3, -2);
+        Vector vr = v1.crossProduct(v2);
+
+        // TC01: Test that length of cross-product is proper (orthogonal vectors taken
+        // for simplicity)
+        assertEquals( v1.length() * v2.length(), vr.length(), 0.0000,"crossProduct() wrong result length");
+
+        // TC02: Test cross-product result orthogonality to its operands
+        assertTrue( isZero(vr.dotProduct(v1)),"crossProduct() result is not orthogonal to 1st operand");
+        assertTrue( isZero(vr.dotProduct(v2)),"crossProduct() result is not orthogonal to 2nd operand");
+
+        // =============== Boundary Values Tests ==================
+        // TC11: test zero vector from cross-productof co-lined vectors
+        Vector v3 = new Vector(-2, -4, -6);
+        assertThrows("crossProduct() for parallel vectors does not throw an exception",
+                IllegalArgumentException.class, () -> v1.crossProduct(v3));
+        // try {
+        //     v1.crossProduct(v2);
+        //     fail("crossProduct() for parallel vectors does not throw an exception");
+        // } catch (Exception e) {}
+    }
+
+
+
+    @Test
+    void dotProduct() {
+    }
+
+    @Test
+    void lengthSquared() {
+    }
+
+    @Test
+    void length() {
+    }
+
+    @Test
+    void normalize() {
+    }
+
+    @Test
+    void normalized() {
+    }
+}

15e34b8d7c8bd831c574a0e7e520964e0d4ef3cb
Shulamit Nahon <shulamitna26@gmail.com>
2021-04-06 23:46:46 +0300

    2nd stage submission

--- tests/geometries/CylinderTest.java
+++ tests/geometries/CylinderTest.java
@@ -1,16 +1,32 @@
 package geometries;
 
 import org.junit.jupiter.api.Test;
-
+import primitives.Point3D;
+import primitives.Ray;
+import primitives.Vector;
 import static org.junit.jupiter.api.Assertions.*;
 
-/**
- * Testing Cylinders
- */
-class CylinderTest {
-
+class CylinderTests {
     @Test
-    void getNormal() {
+    void testGetNormal() {
+        Cylinder cylinder =
+                new Cylinder(new Ray(new Point3D(0,0,0), new Vector(0,0,1)), 1, 1);
 
+        // ============ Equivalence Partitions Tests ==============
+
+        // TC01: the point is on one of the planes
+        Point3D point3D = new Point3D(0.5,0,0);
+        assertEquals(new Vector(new Point3D(0,0,1)),cylinder.getNormal(point3D),
+                "ERROR - TC01: the point is on one of the planes. Bad normal to cylinder");
+
+        // TC02: the point is on the shell
+        Point3D point3D2 = new Point3D(1,0,0.5);
+        assertEquals(new Vector(new Point3D(1,0,0)), cylinder.getNormal(point3D2), "ERROR - TC02: the point is on the shell. Bad normal to cylinder");
+
+        // =============== Boundary Values Tests ==================
+        // TC03:
+        Point3D point3D3 = new Point3D(1,0,0);
+        assertEquals(new Vector(new Point3D(0,0,1)), cylinder.getNormal(point3D), "ERROR - TC03: Bad normal to cylinder");
     }
+
 }

26e2152f1d8e9fe613a92c696ede70d74839cf8e
lea haimovich <haimovit@g.jct.ac.il>
2021-04-13 01:07:14 +0300

    3

--- tests/geometries/GeometriesTest.java
+++ tests/geometries/GeometriesTest.java
@@ -20,7 +20,10 @@ class GeometriesTest {
 
         Geometries g=new Geometries();
         Ray ray=new Ray(new Point3D(1d,1d,1d),new Vector(1d,1d,2d));
+        //Check if the body collection is empty (BVA), then need to return null
         assertEquals(null,g.findIntersections(ray),"ERROR: empty Geometries do not have Intersections ");
+        Intersectable intersectable= new Sphere(1d,new Point3D(2d,3d,-4d));
+        g.add(intersectable);//We added sphere to the collection of geometry   פה הפסקתי, קצת הסתבתכתי....
 
     }
 }

693c73a52279158b52c704bc986cd700aeaaced3
lea haimovich <haimovit@g.jct.ac.il>
2021-04-26 17:57:25 +0300

    ex-4

---
+++ src/elements/Camera.java
@@ -0,0 +1,86 @@
+package elements;
+
+import primitives.Point3D;
+import primitives.Ray;
+import primitives.Vector;
+
+import static primitives.Util.isZero;
+
+public class Camera {
+    final Point3D _p0;
+    final Vector _vTo;
+    final Vector _vUp;
+    final Vector _vRight;
+
+    public Point3D getP0() {
+        return _p0;
+    }
+
+    public Vector getvTo() {
+        return _vTo;
+    }
+
+    public Vector getvUp() {
+        return _vUp;
+    }
+
+    private  double _distance;
+    private  double _width;
+    private  double _height;
+
+
+    public Camera(Point3D p0, Vector vTo, Vector vUp) {
+        _p0 = p0;
+        _vTo = vTo.normalized();
+        _vUp = vUp.normalized();
+        if(!isZero(_vTo.dotProduct(_vUp))){
+            throw new IllegalArgumentException("vUp are orthogonal to vTo");
+        }
+        _vRight=_vTo.crossProduct(_vUp);
+    }
+
+    /**
+     * borrowing from builder pattern
+     * @param width
+     * @param height
+     * @return
+     */
+    public Camera setViewPlaneSize(double width,double height){
+        _width=width;
+        _height=height;
+        return this;
+    }
+
+    public Camera setDistance(double distance){
+        _distance=distance;
+        return this;
+    }
+
+    // constructing a ray passing through pixel(i,j) of the view plane
+    public Ray constructRayThroughPixel(int nX, int nY, int j, int i) {
+        Point3D Pc = _p0.add(_vTo.scale(_distance));
+
+        double Rx = _width / nX;
+        double Ry = _height / nY;
+
+        Point3D Pij = Pc;
+
+        double Xj = (j - (nX - 1) / 2d) * Rx;
+        double Yi = -(i - (nY - 1) / 2d) * Ry;
+
+        if (isZero(Xj) && isZero(Yi)) {
+            return new Ray(_p0, Pij.subtract(_p0));
+        }
+        if (isZero(Xj)) {
+            Pij = Pij.add(_vUp.scale(Yi));
+            return new Ray(_p0, Pij.subtract(_p0));
+        }
+        if (isZero(Yi)) {
+            Pij = Pij.add(_vRight.scale(Xj));
+            return new Ray(_p0, Pij.subtract(_p0));
+        }
+
+        Pij = Pij.add(_vRight.scale(Xj).add(_vUp.scale(Yi)));
+        return new Ray(_p0, Pij.subtract(_p0));
+    }
+}
